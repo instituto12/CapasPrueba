812b186b2f3bd5d0592fe0009f7854de14b4c720
catalinango <catalinango@gmail.com>
2014-10-01 03:05:23 -0300

    Faltaba el SP de busqueda por Cuil, agregamos campos: calle, email y web a la tabla y una consulta para agregar basura a la bd para pruebas rapidamente.

---
+++ 8 Clientes_Add_Col_Calle-Mail-WEB.sql
@@ -0,0 +1,6 @@
+﻿alter table clientes add calle varchar (100) null
+go
+alter table clientes add mail varchar(100) null
+go
+alter table clientes  add web varchar(100) null
+go
---
+++ 9 ALTER_SP_Clientes_insertarRegistro.sql
@@ -0,0 +1,13 @@
+﻿ALTER procedure clientes_insertarRegistro (@idCliente int, @razonsocial varchar, @cuil char, @calle varchar, @mail varchar, @web varchar) as 
+insert into Clientes (idcliente,
+					razonsocial,
+					cuil,
+					calle,
+					mail,
+					web) 
+	values(@idCliente, 
+			@razonsocial,
+			@cuil,
+			@calle,
+			@mail,
+			@web)
