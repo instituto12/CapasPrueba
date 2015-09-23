be598ae5c7d92a035820c82f2eba0bdc31e39502
rpvcgroup <crisense@gmail.com>
2015-09-22 21:14:59 -0300

    Merge branch 'master' of https://github.com/instituto12/CapasPrueba
    Conflicts:
    	15 ALTER SP_clientes_insertarRegistro.sql

---
+++ 15 ALTER SP_clientes_insertarRegistro.sql.REMOTE.1788.sql
@@ -0,0 +1,32 @@
+﻿Esto lo puso diego
+ALTER procedure dbo.clientes_insertarRegistro 
+	@idCliente int, 
+	@razonsocial varchar(100),
+	@cuil char(11),
+	@calle varchar(100),
+	@email varchar(100),
+	@dirWeb varchar(100),
+	@localidad varchar (30),
+	@telefono varchar (100),
+	@telefonocelular varchar(12)
+	
+AS 
+INSERT INTO Clientes (idcliente,
+					razonsocial,
+					cuil,
+					calle,
+					mail,
+					web,
+					localidad,
+					telefono,
+					telefonocelular)
+					 
+	VALUES(@idCliente, 
+			@razonsocial,
+			@cuil,
+			@calle,
+			@email,
+			@dirWeb,
+			@localidad,
+			@telefono,
+			@telefonocelular)
